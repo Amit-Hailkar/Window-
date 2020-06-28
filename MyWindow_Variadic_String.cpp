e7a33320ff5a9ef2403225215b8e1c282311c1ea
Amit-Hailkar <65338992+Amit-Hailkar@users.noreply.github.com>
2020-06-28 21:04:13 +0530

    Add files via upload

---
+++ MyWindow_Variadic_String.cpp
@@ -0,0 +1,79 @@
+// Headers
+#include<windows.h>
+
+//Global Function Declaration
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//Winmain()
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	//Variable Declarations
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("MyApp");
+
+	//Code
+	//initialization of WNDCLASSEX
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	//register above claas
+	RegisterClassEx(&wndclass);
+
+	//create window
+	hwnd = CreateWindow(szAppName,
+		TEXT("MY FIRST WINDOW-Win10_VS2019-:RTR:- AMIT HAILKAR"),
+		WS_MAXIMIZEBOX,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	//message loop
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return((int)msg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	TCHAR str[255];
+	int num1 = 75, num2 = 100;
+	int num3;
+
+	//code
+	switch (iMsg)
+	{
+	case WM_CREATE:
+		num3 = num1 + num2;
+		wsprintf(str, TEXT("SUM OF %d and %d is %d"),num1,num2,num3);
+		MessageBox(hwnd, str, TEXT("MY_SUM"), MB_ICONINFORMATION | MB_OK);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+
